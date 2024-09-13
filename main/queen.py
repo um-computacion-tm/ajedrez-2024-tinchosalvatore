606b294b7033d5d79a5e508fb21180a0bd95201c
Martin Salvatore <martingsalvatore@gmail.com>
2024-09-12 21:23:16 -0300

    probando aprobar code climate

--- main/queen.py
+++ main/queen.py
@@ -6,7 +6,7 @@ from main.movements import ReglasDeMovimientos
 class Queen(Piece):
     def __init__(self, color):
         super().__init__(color)
-        self.__movimientos__ = ReglasDeMovimientos()
+        self.__movimientos_queen__ = ReglasDeMovimientos()
     
     def __str__(self):
         if self.__color__ == "WHITE":
@@ -15,5 +15,5 @@ class Queen(Piece):
             return "♕"
         
     def valid_moves(self, from_row, from_col, to_row, to_col):
-        self.__movimientos__.queen_movement(from_row, from_col, to_row, to_col)
+        self.__movimientos_queen__.queen_movement(from_row, from_col, to_row, to_col)
         return True
